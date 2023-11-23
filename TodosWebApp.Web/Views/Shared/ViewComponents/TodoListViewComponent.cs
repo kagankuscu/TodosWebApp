a048eb26ea30cfeeeee9bfd77d1828d8c6b563c2
kagankuscu <kuscukagan@gmail.com>
2023-11-24 00:06:26 +0300

    Views added some view made ViewComponent

--- TodosWebApp.Web/Views/Shared/ViewComponents/TodoListViewComponent.cs
+++ TodosWebApp.Web/Views/Shared/ViewComponents/TodoListViewComponent.cs
@@ -2,16 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using TodosWebApp.BusinessLogic;
+using TodosWebApp.DataAccess.Entities;
 using TodosWebApp.Web.ViewModels;
 
 namespace TodosWebApp.Web.Views.Shared.ViewComponents
 {
     public class TodoListViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync(TodoViewModel todo)
+        private readonly ITodoService _todoService;
+        private readonly IMapper _mapper;
+
+        public TodoListViewComponent(ITodoService todoService, IMapper mapper)
         {
-            return View(todo);
+            _todoService = todoService;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int type = 1)
+        {
+            List<Todo> todos = await _todoService.GetAllAsync();
+            List<TodoViewModel> todoViewModels = _mapper.Map<List<TodoViewModel>>(todos);
+
+            if (type == 1)
+            {
+                todoViewModels = _mapper.Map<List<TodoViewModel>>(todos.OrderByDescending(x=>x.CreatedDate).ToList());
+            }
+            if (type == 2)
+            {
+                todoViewModels = _mapper.Map<List<TodoViewModel>>(todos.OrderBy(x=>x.CreatedDate).TakeWhile(x=>x.CreatedDate < DateTime.Now).OrderByDescending(x=>x.CreatedDate).ToList());
+            }
+            return View(todoViewModels);
         }
     }
 }
