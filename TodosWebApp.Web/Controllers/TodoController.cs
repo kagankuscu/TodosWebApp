bbcaf1b87732062aaacf51b807fd4a58ba2aa20b
kagankuscu <kuscukagan@gmail.com>
2023-11-23 18:11:00 +0300

    mapper added and change Entity to ViewModel

--- TodosWebApp.Web/Controllers/TodoController.cs
+++ TodosWebApp.Web/Controllers/TodoController.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using TodosWebApp.BusinessLogic;
 using TodosWebApp.DataAccess.Entities;
+using TodosWebApp.Web.ViewModels;
 
 namespace TodosWebApp.Web.Controllers
 {
@@ -12,10 +14,12 @@ namespace TodosWebApp.Web.Controllers
     public class TodoController : Controller
     {
         private readonly ITodoService _todoService;
+        private readonly IMapper _mapper;
 
-        public TodoController(ITodoService todoService)
+        public TodoController(ITodoService todoService, IMapper mapper)
         {
             _todoService = todoService;
+            _mapper = mapper;
         }
 
         [Route("/")]
@@ -23,14 +27,18 @@ namespace TodosWebApp.Web.Controllers
         [Route("/todo/index")]
         public async Task<IActionResult> Index()
         {
-            List<Todo> todo = await _todoService.GetAllAsync();
-            return View(todo.OrderByDescending(x=>x.CreatedDate).ToList());
+            List<Todo> todos = await _todoService.GetAllAsync();
+            todos = todos.OrderByDescending(x=>x.CreatedDate).ToList();
+            List<TodoViewModel> todosVM = _mapper.Map<List<TodoViewModel>>(todos);
+            return View(todosVM);
         }
 
         public async Task<IActionResult> History()
         {
-            List<Todo> todo = await _todoService.GetAllAsync();
-            return View(todo);
+            List<Todo> todos = await _todoService.GetAllAsync();
+            todos = todos.OrderByDescending(x=>x.CreatedDate).ToList();
+            List<TodoViewModel> todosVM = _mapper.Map<List<TodoViewModel>>(todos);
+            return View(todosVM);
         }
     }
 }
