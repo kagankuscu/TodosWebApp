65bc48bbf4f5410ced74f5d2a42a31bd5564ee5a
kagankuscu <kuscukagan@gmail.com>
2023-11-23 16:56:50 +0300

    Presentation Layer

---
+++ TodosWebApp.Web/Views/Shared/ViewComponents/TodoListViewComponent.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodosWebApp.Web.Views.Shared.ViewComponents
+{
+    public class TodoListViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
