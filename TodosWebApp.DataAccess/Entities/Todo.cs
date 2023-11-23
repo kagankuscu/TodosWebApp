46410f5c50b58ae57dca75bd68f4155c45753b2b
kagankuscu <kuscukagan@gmail.com>
2023-11-23 15:44:12 +0300

    DataAccess Layer, Todo Entity, ITodoRepository and TodoRepository

---
+++ TodosWebApp.DataAccess/Entities/Todo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodosWebApp.DataAccess.Entities
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Task { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
