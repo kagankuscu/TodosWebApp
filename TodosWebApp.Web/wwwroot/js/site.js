494b0b396ba0650ecb17fdf0af9aba9fbfa33ca5
kagankuscu <kuscukagan@gmail.com>
2023-11-24 14:23:56 +0300

    Moved to General Js file

--- TodosWebApp.Web/wwwroot/js/site.js
+++ TodosWebApp.Web/wwwroot/js/site.js
@@ -1,4 +1,25 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function MyFunction(id)
+{
+    jsonData = {
+        id: id,
+        name: $("#name"+id).text(),
+        isDone: $("#isDone"+id).is(":checked"),
+        dueDate: $("#dueDate"+id).text(),
+        createdDate: $("#createdDate" + id).text()
+    }
+
+    console.log(jsonData);
+
+    $.ajax({
+        type: "POST",
+        url: "/todo/updateisdone",
+        data: jsonData,
+        error: function(error)
+        {
+            alert(error);
+        }
+    });
+}
