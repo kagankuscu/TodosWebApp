a1fe0658fcef2eed0cf3c7a13520af109619c9e2
kagankuscu <kuscukagan@gmail.com>
2023-11-29 14:23:15 +0300

    Generic Repository Added.

---
+++ TodosWebApp.BusinessLogic/Shared/Concrete/UnitOfWork.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodosWebApp.BusinessLogic.Shared.Abstract;
+using TodosWebApp.DataAccess.Data;
+using TodosWebApp.DataAccess.Entities;
+
+namespace TodosWebApp.BusinessLogic.Shared.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public IRepository<Todo> Todos { get; private set; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Todos = new Repository<Todo>(_context);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
