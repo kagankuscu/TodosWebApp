6db5352888552b9750d2dc0bfed3944d7ab50720
kagankuscu <kuscukagan@gmail.com>
2023-12-03 13:25:12 +0300

    Register properties added.

--- TodosWebApp.Web/ViewModels/UserViewModel.cs
+++ TodosWebApp.Web/ViewModels/UserViewModel.cs
@@ -4,6 +4,10 @@ namespace TodosWebApp.Web;
 
 public class UserViewModel : BaseModel
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string Email { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? ConfirmPassword { get; set; }
 }
