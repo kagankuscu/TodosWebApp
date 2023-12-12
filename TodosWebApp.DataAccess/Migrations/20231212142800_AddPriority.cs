db8f702c0ad37ebf806964f918482641e26e06dd
kagankuscu <kuscukagan@gmail.com>
2023-12-12 17:35:43 +0300

    New Migration PriorityType

---
+++ TodosWebApp.DataAccess/Migrations/20231212142800_AddPriority.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodosWebApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriority : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriorityId",
+                table: "Todos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "PriorityTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriorityTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Priorities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Priorities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Priorities_PriorityTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "PriorityTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_PriorityId",
+                table: "Todos",
+                column: "PriorityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Priorities_TypeId",
+                table: "Priorities",
+                column: "TypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Priorities_PriorityId",
+                table: "Todos",
+                column: "PriorityId",
+                principalTable: "Priorities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Priorities_PriorityId",
+                table: "Todos");
+
+            migrationBuilder.DropTable(
+                name: "Priorities");
+
+            migrationBuilder.DropTable(
+                name: "PriorityTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Todos_PriorityId",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PriorityId",
+                table: "Todos");
+        }
+    }
+}
