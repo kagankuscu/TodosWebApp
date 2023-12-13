928bc1d29c3e86dfe71328bde82f04f3905719ef
kagankuscu <kuscukagan@gmail.com>
2023-12-13 17:24:48 +0300

    Add UnitOfWork new Entities

--- TodosWebApp.BusinessLogic/Shared/Concrete/UnitOfWork.cs
+++ TodosWebApp.BusinessLogic/Shared/Concrete/UnitOfWork.cs
@@ -17,6 +17,8 @@ namespace TodosWebApp.BusinessLogic.Shared.Concrete
         public IRepository<User> Users { get; private set; }
         public IRepository<Priority> Priorities { get; private set; }
         public IRepository<PriorityType> PriorityTypes { get; private set; }
+        public IRepository<Tag> Tags { get; private set; }
+        public IRepository<TagTypes> TagTypes { get; private set; }
 
         public UnitOfWork(AppDbContext context)
         {
@@ -25,6 +27,8 @@ namespace TodosWebApp.BusinessLogic.Shared.Concrete
             Users = new Repository<User>(_context);
             Priorities = new Repository<Priority>(_context);
             PriorityTypes = new Repository<PriorityType>(_context);
+            Tags = new Repository<Tag>(_context);
+            TagTypes = new Repository<TagTypes>(_context);
         }
 
         public void Save()
