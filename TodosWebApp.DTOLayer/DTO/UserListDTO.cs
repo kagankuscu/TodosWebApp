5865e62bae6d459cdbf89e6862cdf9c7b2a86302
kagankuscu <kuscukagan@gmail.com>
2023-12-18 18:25:38 +0200

    Edit property UserDTO

--- TodosWebApp.DTOLayer/DTO/UserListDTO.cs
+++ TodosWebApp.DTOLayer/DTO/UserListDTO.cs
@@ -2,20 +2,13 @@
 
 public class UserListDTO
 {
-    public int Id { get; set; }
-    public string Username { get; set; }
+    public int Id { get; set; }    
     public string Email { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Fullname 
-    { 
-        get 
-        { 
-            return $"{FirstName} {LastName}";
-        }
-    }
+    public string Fullname { get; set; }
     public int TotalTask { get; set; }
     public int CompletedTask { get; set; }
+    public int UnCompletedTask { get; set; }
+    public int DeletedTask { get; set; }
     public bool IsAdmin { get; set; }
     public bool IsActive { get; set; }
 }
