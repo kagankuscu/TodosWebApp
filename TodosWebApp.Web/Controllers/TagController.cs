c448758f25093ad5117e8d68717aea47dc6107fd
kagankuscu <kuscukagan@gmail.com>
2023-12-19 14:56:39 +0200

    Add controller save tag

--- TodosWebApp.Web/Controllers/TagController.cs
+++ TodosWebApp.Web/Controllers/TagController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TodosWebApp.BusinessLogic.Shared.Abstract;
+using TodosWebApp.DTOLayer.DTO;
+using TodosWebApp.Model.Entities;
 
 namespace TodosWebApp.Web.Controllers
 {
@@ -24,6 +26,26 @@ namespace TodosWebApp.Web.Controllers
         public IActionResult GetAllTags()
         {
             return Json(_unitOfWork.Tags.GetAll().ToList());
+        }
+        [HttpPost]
+        public IActionResult SaveTag(TagDTO tagDTO)
+        {
+            try
+            {
+
+                _unitOfWork.Tags.Add(new Tag 
+                {
+                    Name = tagDTO.Name,
+                    Color = tagDTO.Color
+                });
+                _unitOfWork.Save();
+                return Ok("Tag saved successfully");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while saving tag");
+                return BadRequest("Something went wrong. Save unsuccessful. Please try againg");
+            }
         }
     }
 }
