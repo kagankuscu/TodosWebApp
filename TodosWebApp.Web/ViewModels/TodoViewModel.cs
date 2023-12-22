ac8edce30c389b6c212500e42c7f1e985e2430ee
kagankuscu <kuscukagan@gmail.com>
2023-12-22 20:09:54 +0200

    nullable warning refactor

--- TodosWebApp.Web/ViewModels/TodoViewModel.cs
+++ TodosWebApp.Web/ViewModels/TodoViewModel.cs
@@ -12,7 +12,6 @@ namespace TodosWebApp.Web.ViewModels
         
         public int Id { get; set; }
         public string? Name { get; set; }
-        // public DateTime CreatedDate { get; set; }
         public DateTime DueDate { get; set; }
         public bool IsDone { get; set; }
         public int PriorityId { get; set; }
