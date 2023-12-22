fcada6d1c0e800cced5f2451d2ebf5217fb9b18f
kagankuscu <kuscukagan@gmail.com>
2023-12-22 20:16:34 +0200

    Refactor null

--- TodosWebApp.DTOLayer/DTO/TagDTO.cs
+++ TodosWebApp.DTOLayer/DTO/TagDTO.cs
@@ -9,7 +9,7 @@ namespace TodosWebApp.DTOLayer.DTO
     public class TagDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Color { get; set; }
+        public string Name { get; set; } = null!;
+        public string Color { get; set; } = null!;
     }
 }
